d6b011cdf45275ad143dbe3a9f7f1738c5b4f24f
Thiago Goulart <thiago.goulart@programmers.com.br>
2019-10-16 23:37:22 -0300

    first commit

---
+++ ClockInOut.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace clock_in_out
+{
+    public class ClockInOut
+    {
+        ChromeDriver driver;
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        [Fact]
+        private void PunchCard()
+        {
+            var chromeDriverDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //chromeOptions.AddArguments("headless");
+            driver = new ChromeDriver(chromeDriverDirectory, chromeOptions);
+            driver.Navigate().GoToUrl("https://pontoeletronico.programmers.com.br/portalrh/");
+
+            driver.FindElementById("ext-gen51").SendKeys("your email here");
+            driver.FindElementById("ext-gen53").SendKeys("your password here");
+            driver.FindElementById("ext-gen55").Click();
+
+        }
+    }
+}
+    
